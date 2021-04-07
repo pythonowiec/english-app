9b29d9ae56d37e018593e97efc1fc8f14ff0cf94
pythonowiec <kuba.dobosz2004@gmail.com>
2021-04-07 16:02:12 +0200

    Configure database

--- public/js/app.js
+++ public/js/app.js
@@ -38141,9 +38141,9 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\tomynewlaptop\friends projects\english app with classmates\english-app\resources\js\app.js */"./resources/js/app.js");
-__webpack_require__(/*! D:\tomynewlaptop\friends projects\english app with classmates\english-app\resources\sass\app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! D:\tomynewlaptop\friends projects\english app with classmates\english-app\resources\sass\main.scss */"./resources/sass/main.scss");
+__webpack_require__(/*! C:\Users\Jakub\OneDrive\Pulpit\english-app\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! C:\Users\Jakub\OneDrive\Pulpit\english-app\resources\sass\app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! C:\Users\Jakub\OneDrive\Pulpit\english-app\resources\sass\main.scss */"./resources/sass/main.scss");
 
 
 /***/ })
