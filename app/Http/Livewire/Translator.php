6a08276960f70bede963051bb3e7b6aef16eb0a7
pythonowiec <kuba.dobosz2004@gmail.com>
2021-04-13 09:37:54 +0200

    Build languages list

--- app/Http/Livewire/Translator.php
+++ app/Http/Livewire/Translator.php
@@ -10,30 +10,30 @@ use \Statickidz\GoogleTranslate;
 class Translator extends Component
 {
 
-    //text  to translate
-    public $message;
-    //translation result
-    public $result;
-    //GoogleTransalte class instance
-    private $trans;
+    // //text  to translate
+    // public $message;
+    // //translation result
+    // public $result;
+    // //GoogleTransalte class instance
+    // private $trans;
 
-    //lanuguages codes {source} | {target}
-    public $source;
-    public $target;
+    // //lanuguages codes {source} | {target}
+    // public $source;
+    // public $target;
 
     public function __construct(){
 
-        $this->trans = new GoogleTranslate();
+        // $this->trans = new GoogleTranslate();
 
     }
 
     public function render()
     {
-            if ((!$this->message == "") and (!$this->source == "") and (!$this->target == "")){
-                $this->result = $this->trans->translate($this->source, $this->target, $this->message);
-            }else{
-                $this->message="";
-            }
+            // if ((!$this->message == "") and (!$this->source == "") and (!$this->target == "")){
+            //     $this->result = $this->trans->translate($this->source, $this->target, $this->message);
+            // }else{
+            //     $this->message="";
+            // }
 
         return view('livewire.translator');
     }
--- resources/views/livewire/translator.blade.php
+++ resources/views/livewire/translator.blade.php
@@ -1,9 +1,8 @@
 <div class="input-group textarea-translate-group mb-3">
-    <input type="text" wire:model="source" />
-    <input type="text" wire:model="target" />
-    <textarea type="text"  name="text_input" class="form-control textarea-translate" wire:model.debounce.300ms="message" placeholder="Translate"></textarea>
-    <textarea type="text"  name="text_input_translated" class="form-control textarea-translate" placeholder="Translate" disabled>{{$result ?? ""}}</textarea>
-
+    {{-- <input type="text" wire:model="source" />
+    <input type="text" wire:model="target" /> --}}
+    <textarea type="text"  name="text_input" class="form-control textarea-translate" style="border-radius: 0 0 0 .25rem !important;" wire:model.debounce.300ms="message" placeholder="Translate"></textarea>
+    <textarea type="text"  name="text_input_translated" class="form-control textarea-translate" style="border-radius: 0 0 .25rem 0 !important;" placeholder="Translate" disabled>{{$result ?? ""}}</textarea>
 </div>
 
 
